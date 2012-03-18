170edbce78a6fa071b263422bc64bac7ab051fbd
van-de-bugger <van-de-bugger@localhost>
2012-03-18 21:00:50 +0000

    Follow-up to r14108: <code> tags ised in description message.

--- WhiteSpace.i18n.php
+++ WhiteSpace.i18n.php
@@ -12,5 +12,5 @@ $messages = array();
  * @author Van de Bugger
  */
 $messages['en'] = array(
-	'whitespace-desc' => 'Tags to control whitespace: &lt;dws/&gt; discards itself and all whitespace (including newlines) after the tag, &lt;nl/&gt; replaces itself with newline.',
+	'whitespace-desc' => 'Tags to control whitespace: <code>&lt;dws/&gt;</code> discards itself and all whitespace (including newlines) after the tag, <code>&lt;nl/&gt;</code> replaces itself with newline.',
 );
